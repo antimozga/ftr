23d8a85c739fc7c1589695bd11a6135c7e9d2293
antimozga <antimozga@vtomske.net>
2020-03-10 22:36:35 +0400

    disabled try, pln

--- exchangerate-ecb.php
+++ exchangerate-ecb.php
@@ -17,8 +17,8 @@ echo '<div class="rates" style="width:10em;">';
 echo '<span class="w_title">Курс <a class="rates_link" href="https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html" target="_blank">ECB</a></span><br/>';
 echo '<span style="width:4em;display:inline-block;">CNY 元</span>'.getRate($json['rates']['CNY']).'<br/>';
 echo '<span style="width:4em;display:inline-block;">EUR €</span>'.getRate($json['rates']['EUR']).'<br/>';
-echo '<span style="width:4em;display:inline-block;">PLN zł</span>'.getRate($json['rates']['PLN']).'<br/>';
-echo '<span style="width:4em;display:inline-block;">TRY ₺</span>'.getRate($json['rates']['TRY']).'<br/>';
+//echo '<span style="width:4em;display:inline-block;">PLN zł</span>'.getRate($json['rates']['PLN']).'<br/>';
+//echo '<span style="width:4em;display:inline-block;">TRY ₺</span>'.getRate($json['rates']['TRY']).'<br/>';
 echo '<span style="width:4em;display:inline-block;">USD $</span>'.getRate($json['rates']['USD']).'<br/>';
 echo '</div>';
 ?>
