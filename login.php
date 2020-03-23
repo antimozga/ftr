a92689c2f73d236a6dc57609745ab0a748ff68d5
antimozga <antimozga@vtomske.net>
2020-03-23 10:51:56 +0400

    login window fix

--- login.php
+++ login.php
@@ -7,14 +7,16 @@ session_start();
 include('funcs.php');
 
 echo '
-<div style="display:table; width:100%; height:100%;">
+<div class="modal-content-window" style="display:table;">
 	<form action="" method="post" style="display:table-cell; vertical-align:middle; text-align:center;">
+	    <div style="font-size: 200%;font-weight: bold;padding-bottom: 20px;">Вход</div>
 	    <input type="hidden" name="event" value="login"/>
 	    <div class="form_box_text">
 		<input type="text" autocomplete="username" style="width:40%;" name="user[name]" onfocus="if(this.value == \'Имя\') { this.value = \'\'; }" value="Имя"/>
 		<input type="password" autocomplete="current-password" style="width:40%;" name="user[password]" onfocus="if(this.value == \'Пароль\') { this.value = \'\'; }" value="Пароль"/>
 		<input class="btn_group_sel" type="submit" value="&nbsp;" />
 	    </div>
+	    <div style="padding-top: 10px;"><a href="?reg=1">Регистрация</a></div>
 	</form>
 </div>';
 
