bea4ded7c162e105841c2f5acd42a5399f8bc5b3
antimozga <antimozga@vtomske.net>
2020-04-01 02:30:01 +0400

    check if no signature

--- pagerchat.js
+++ pagerchat.js
@@ -38,7 +38,13 @@ function pagerInsertPost(el, data)
 		    privateKeys: [privateKey]                            // for decryption
 		});
 
-		if ((signatures[0].keyid.toHex() === publicKeys[0][0].keyPacket.keyid.toHex() && data.l === userName) ||
+		if (typeof signatures[0] === 'undefined') {
+		    //lock = '&#x1F513;';
+		    lock = `<svg viewBox="0 0 20 20" width="16px" class="svg_icon_error">
+<title>Отсутствует подпись отправителя</title>
+<path d="M4 8V6a6 6 0 1 1 12 0h-3v2h4a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/>
+</svg>`;
+		} else if ((signatures[0].keyid.toHex() === publicKeys[0][0].keyPacket.keyid.toHex() && data.l === userName) ||
 		    (signatures[0].keyid.toHex() === publicKeys[1][0].keyPacket.keyid.toHex() && data.l !== userName)) {
 		    //lock = '&#x1f512;';
 		    lock = `<svg viewBox="0 0 20 20" width="16px" class="svg_icon">
